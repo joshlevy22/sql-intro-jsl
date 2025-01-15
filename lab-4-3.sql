6045a76746e2c68ae52bff8895c7298b2e678754
Josh Levy <josh.levy@kellogg.northwestern.edu>
2025-01-15 22:11:32 +0000

    made the code

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -6,4 +6,11 @@
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
 
+.mode columns
+.headers on
 
+SELECT name, first_name, last_name, max(home_runs)
+FROM stats
+INNER JOIN players ON players.id = stats.player_id
+INNER JOIN teams ON teams.id = stats.team_id
+WHERE year = "2019"; 
